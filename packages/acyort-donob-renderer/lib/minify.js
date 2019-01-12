08b82149c5b2ca312c8d1f2607003fddaca2562d
zhengzhirong <zhengzwing@gmail.com>
2019-01-12 13:10:36 +0800

    feat: 🎸 (renderer)minify html&js&css

--- packages/acyort-donob-renderer/lib/minify.js
+++ packages/acyort-donob-renderer/lib/minify.js
@@ -1,10 +1,29 @@
-const minify = require('html-minifier').minify
-const fs = require('fs-extra')
+// const minify = require('html-minifier').minify
+const gulp = require('gulp')
+const minifier = require('gulp-htmlmin')
+const uglify = require('gulp-uglify')
+const postcss = require('gulp-postcss')
+const cssnano = require('cssnano')
+const autoprefixer = require('autoprefixer')
 
-async function min(path) {
-  const content = await fs.readFile(path, {
-    encoding: 'utf-8',
+module.exports = (publicDir) => {
+  gulp.task('html', () => gulp
+    .src(`${publicDir}/**/*.html`)
+    .pipe(minifier({ collapseWhitespace: true }))
+    .pipe(gulp.dest(publicDir)))
+  gulp.task('js', () => gulp
+    .src(`${publicDir}/**/*.js`)
+    .pipe(uglify())
+    .pipe(gulp.dest(publicDir)))
+  gulp.task('css', () => gulp
+    .src(`${publicDir}/**/*.css`)
+    .pipe(postcss([autoprefixer(), cssnano({ preset: 'default' })]))
+    .pipe(gulp.dest(publicDir)))
+  return new Promise((res) => {
+    gulp.task('default', gulp.series('html', 'js', 'css', () => {
+      res()
+    }))
+    const ins = gulp.task('default')
+    ins.apply(gulp)
   })
-  const miniHTML = minify(content)
-  fs.writeFile(path, miniHTML)
 }
--- packages/acyort-donob-renderer/test/index.spec.js
+++ packages/acyort-donob-renderer/test/index.spec.js
@@ -1,11 +1,18 @@
 const get = jest.fn().mockResolvedValue([])
 const copySource = jest.fn().mockReturnValue(1)
-const plugins = require('..')
+const min = jest.fn()
+const { join } = require('path')
+
+jest.doMock('../lib/minify', () => min)
 
 const cache = []
 const register = jest.fn((fn) => {
   cache.push(fn)
 })
+const base = './fdsaf'
+const publicDir = 'fda'
+const plugins = require('..')
+
 describe('test register plugins', () => {
   plugins({
     store: {
@@ -15,13 +22,23 @@ describe('test register plugins', () => {
     workflow: {
       register,
     },
+    config: {
+      base,
+      public: publicDir,
+    },
   })
   it('test workflow register', () => {
     expect(register).toBeCalledTimes(1)
     expect(cache).toHaveLength(1)
   })
   it('test workflow exec', async () => {
-    await cache[0]()
+    await cache[0]({
+      config: {
+        base, public: publicDir,
+      },
+    })
     expect(copySource).toBeCalledTimes(1)
+    expect(min).toBeCalledTimes(1)
+    expect(min).toBeCalledWith(join(base, publicDir))
   })
 })
