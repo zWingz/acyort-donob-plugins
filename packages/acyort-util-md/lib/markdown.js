1d9f35b112b386949947a8c130d31a746f820c79
zWingz <zhengzwing@gmail.com>
2019-03-01 15:17:42 +0800

    feat: 🎸 support custom-block use ':::'

--- packages/acyort-util-md/lib/markdown.js
+++ packages/acyort-util-md/lib/markdown.js
@@ -2,6 +2,7 @@ const remark = require('remark')
 const slug = require('remark-slug')
 const html = require('remark-html')
 const autoheading = require('remark-autolink-headings')
+const container = require('remark-container')
 const highlight = require('./highlight')
 const prettier = require('./prettier')
 const heading = require('./headings')
@@ -20,6 +21,9 @@ const defaultOptions = {
 function parseMd(content, opt = {}) {
   const { highlightOpt } = { ...defaultOptions, ...opt }
   const c = remark()
+    .use(container, {
+      className: 'custom-block',
+    })
     .use([slug, heading, autoheading])
     .use(prettier)
     .use(highlight, highlightOpt)
@@ -27,4 +31,5 @@ function parseMd(content, opt = {}) {
     .processSync(content)
   return c.contents
 }
+
 module.exports.parseMd = parseMd
